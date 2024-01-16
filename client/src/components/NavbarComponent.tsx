a0352f153a72b1d9d6837db08c9052a8a700e48e
Deo silaen <deokeldisilaen@gmail.com>
2024-01-16 23:05:57 +0700

    lanjut

--- client/src/components/NavbarComponent.tsx
+++ client/src/components/NavbarComponent.tsx
@@ -2,8 +2,10 @@ import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
 import calculator from "../assets/images/calculator.png";
+import feedback from "../assets/images/like.gif";
 // import { Popover, Transition } from "@headlessui/react";
 import Pop from "@/components/PopoverS";
+import profile from "../assets/images/profile.gif";
 const navigation = [
   { name: "Home", href: "#", current: true },
   { name: "Calculator", href: "#", current: false },
@@ -69,18 +71,23 @@ const NavbarComponent = () => {
                 >
                   <span className="absolute -inset-1.5" />
                   <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
+                  {/* <BellIcon className="h-6 w-6" aria-hidden="true" /> */}
+                  <img
+                    src={feedback}
+                    className="h-9 w-10 bg-transparent"
+                    alt=""
+                  />
                 </button>
 
                 {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
                   <div>
-                    <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                    <Menu.Button className="relative flex rounded-full border-2 border-gray-500 bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                       <span className="absolute -inset-1.5" />
                       <span className="sr-only">Open user menu</span>
                       <img
                         className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                        src={profile}
                         alt=""
                       />
                     </Menu.Button>
