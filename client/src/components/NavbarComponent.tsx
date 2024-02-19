31534aa965299b9ca5e8c157523051ca4e684271
deoosilaen <deokeldisilaen@gmail.com>
2024-02-19 12:26:15 +0700

    selesai

--- client/src/components/NavbarComponent.tsx
+++ client/src/components/NavbarComponent.tsx
@@ -162,7 +162,7 @@ const NavbarComponent = () => {
     >
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="mx-auto py-2 max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 {/* Mobile menu button*/}
